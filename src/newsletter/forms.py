d256900a7090044ba4a769785318ab14d1f3462c
saurabhs92 <saurabhs92@gmail.com>
2017-05-29 20:13:25 +0530

    Added a basic template and form.

--- src/newsletter/forms.py
+++ src/newsletter/forms.py
@@ -6,3 +6,18 @@ class SignUpForm(forms.ModelForm):
         model = SignUp
         fields = ['full_name', 'email']
         
+    def clean_email(self):
+        print (self.cleaned_data)
+        email = self.cleaned_data.get('email')
+        email_base, provider = email.split('@')
+        domain, extension = provider.split('.')
+        if extension not in ['edu', 'com']:
+            raise forms.ValidationError('Enter a valid email extension')
+        return email
+
+    def clean_full_name(self):
+        full_name = self.cleaned_data.get('full_name')
+        # Validate name
+        return full_name
+
+        
