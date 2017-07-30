86a5ba071a0af5a29b0bc09e9db87aaaeea87188
saurabhs92 <saurabhs92@gmail.com>
2017-07-31 00:15:17 +0530

    Removed form from context if form data is valid.

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -4,23 +4,20 @@ from .forms import SignUpForm
 
 def home(request):
     title = 'Welcome'
-    #if request.user.is_authenticated():
-    #    title = 'Hi %s' %(request.user)
-
-    if request.method == "POST":
-        print (request.POST)
-        
     form = SignUpForm(request.POST or None)
-    if form.is_valid():
-        instance = form.save(commit=False)
-        if not instance.full_name:
-            instance.full_name = "Undefined Full Name"
-        instance.save()
-        print (instance.full_name)
-        print (instance.email)
-        print (instance.timestamp)
+    
     context = {
         'title': title,
         'form': form,
     }
+
+    if form.is_valid():
+        # form.save()
+        instance = form.save(commit=False)
+        if not instance.full_name:
+            instance.full_name = "Undefined Full Name"
+        instance.save()
+        context = {
+            'title': "Thank You for registering"
+        }
     return render(request, "home.html", context)
