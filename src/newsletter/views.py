e1d3141626cab4fa04705faf3fd4c79a5468ef60
saurabhs92 <saurabhs92@gmail.com>
2017-07-31 01:31:09 +0530

    Added a Contact form in /contact/ url.

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import SignUpForm
+from .forms import ContactForm, SignUpForm
 # Create your views here.
 
 def home(request):
@@ -21,3 +21,18 @@ def home(request):
             'title': "Thank You for registering"
         }
     return render(request, "home.html", context)
+
+def contact(request):
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        for key in form.cleaned_data:
+            value = form.cleaned_data.get(key)
+            print (key, value)
+            
+        email = form.cleaned_data.get('email')
+        full_name = form.cleaned_data.get('full_name')
+        message = form.cleaned_data.get('message')        
+    context = {
+        'form': form,
+    }
+    return render(request, "contact.html", context)
