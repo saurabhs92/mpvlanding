5050d4d0ded4eaf034833491fb7db82004515079
saurabhs92 <saurabhs92@gmail.com>
2017-07-31 00:08:05 +0530

    Sign up form data is now saved in db.

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -9,10 +9,18 @@ def home(request):
 
     if request.method == "POST":
         print (request.POST)
-    
+        
     form = SignUpForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        if not instance.full_name:
+            instance.full_name = "Undefined Full Name"
+        instance.save()
+        print (instance.full_name)
+        print (instance.email)
+        print (instance.timestamp)
     context = {
-        'context_title': title,
+        'title': title,
         'form': form,
     }
     return render(request, "home.html", context)
