7750e380fbada4f36419ffea81d2781ba45cecbe
saurabhs92 <saurabhs92@gmail.com>
2017-08-01 00:31:42 +0530

    Added mail functionality on submitting contact msg.

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from django.shortcuts import render
 from .forms import ContactForm, SignUpForm
 # Create your views here.
@@ -25,13 +27,32 @@ def home(request):
 def contact(request):
     form = ContactForm(request.POST or None)
     if form.is_valid():
-        for key in form.cleaned_data:
-            value = form.cleaned_data.get(key)
-            print (key, value)
+        #for key in form.cleaned_data:
+        #    value = form.cleaned_data.get(key)
+        #    print (key, value)
             
-        email = form.cleaned_data.get('email')
-        full_name = form.cleaned_data.get('full_name')
-        message = form.cleaned_data.get('message')        
+        form_email = form.cleaned_data.get('email')
+        form_full_name = form.cleaned_data.get('full_name')
+        form_message = form.cleaned_data.get('message')
+
+        subject = 'Site Contact Form'
+        from_email = settings.EMAIL_HOST_USER
+        to_email = [from_email, 'yourotheremail@mail.com']
+        contact_message = "%s: %s via %s" % (
+            form_full_name,
+            form_message,
+            form_email)
+        some_html_message = """
+        <h1> hello </h1>
+        """
+
+        send_mail(subject,
+                  contact_message,
+                  from_email,
+                  to_email,
+                  html_message=some_html_message,
+                  fail_silently=False)
+        
     context = {
         'form': form,
     }
